35b4a8dd19025c39deb0612567fd00e246a79554
Sarah Heidmann <sarah.heidmann@gmail.com>
2020-06-02 15:39:33 -0400

    north arrow and scale bar

--- scripts/bbmm_plotting.R
+++ scripts/bbmm_plotting.R
@@ -45,13 +45,33 @@ XMax <- 291283
 YMin <- 2028438
 YMax <- 2029931
 
+# Set parameters for scale bar
+anchor_s <- data.frame(x=289860,y=2028750) # Anchor point (center left)
+ht_s <- 25 # height of the ticks (total height is double)
+scalebar <- data.frame(x=c(anchor_s$x,anchor_s$x,anchor_s$x,# left tick
+                           anchor_s$x+1000, # line
+                           anchor_s$x+1000,anchor_s$x+1000), # right tick
+                       y=c(anchor_s$y+ht_s,anchor_s$y-ht_s,anchor_s$y,# left tick
+                           anchor_s$y, # line
+                           anchor_s$y+ht_s,anchor_s$y-ht_s))  # right tick
+# Set parameters for North arrow
+anchor_n <-data.frame(x=291200,y=2029900)
+ht_n <- 150 # height of the arrow
+wd_n <- 100 # width of the arrow
+northarrow <- data.frame(x=c(anchor_n$x, anchor_n$x-(wd_n/2), # top, left bottom
+                             anchor_n$x, anchor_n$x+(wd_n/2), # mid, right bottom
+                             anchor_n$x), # top
+                         y=c(anchor_n$y, anchor_n$y-ht_n, # top, left bottom
+                             anchor_n$y-(ht_n*2/3),  # mid
+                             anchor_n$y-ht_n, anchor_n$y)) # right bottom, top
+
 # Construct the plot
 ggplot(data=mnb_bbmm) +
         # Fill the raster
         geom_raster(aes(x=x,y=y,fill=ud)) +
         scale_fill_gradientn("Probability",colors=rev(brewer.pal(9, "RdYlBu"))) +
         # Plot the land
-        geom_polygon(data = landSTTSTJdf, aes(x_UTM20N, y_UTM20N, group = group),
+        geom_polygon(data = landSTTSTJ, aes(x_UTM20N, y_UTM20N, group = group),
                      fill = "black") +
         # Set a consistent extent
         coord_equal(xlim = c(XMin,XMax),
@@ -61,6 +81,12 @@ ggplot(data=mnb_bbmm) +
         geom_text(size = 6, color = "white", # label the panels
                   aes(x=290600,y=2029850, family = "Times New Roman", 
                       label = substr(trans,10,14))) +
+        # Add the scale bar
+        geom_path(data = scalebar, aes(x=x,y=y), color = "black") +
+        geom_text(data = anchor_s, aes(x=x+150,y=y-80), color = "black", 
+                  label = "1 km", family = "Times New Roman", size = 4) +
+        # Add the north arrow
+        geom_polygon(data = northarrow, aes(x=x,y=y), fill = "white") +
         # No axis labels
         xlab("")+
         ylab("")+
